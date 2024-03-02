ec3aa504990435c72822e6be3bc2413180e87d9d
Swapnil Shandilya <92042583+Swap2k2@users.noreply.github.com>
2024-03-02 16:47:46 +0530

    final deployment

--- index.js
+++ index.js
@@ -6,12 +6,33 @@ const messagesRoute = require("./routes/messagesRoute.js");
 const app = express();
 require("dotenv").config();
 const socket = require("socket.io");
-
+const path = require("path")
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messagesRoute);
+
+// --------------------------Deployment----------------------------
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname1,"/client/build")));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
+    })
+}else{
+    app.get("/", (req,res)=>{
+        res.send("API is running successfully");
+    });
+}
+
+
+// ------------------------------------------------------------------
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
