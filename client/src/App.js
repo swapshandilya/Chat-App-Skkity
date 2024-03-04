dfa031aa470d2389767d65759a454774b9796fab
Swapnil Shandilya <92042583+Swap2k2@users.noreply.github.com>
2024-03-04 12:21:31 +0530

    changed default page to Login

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Register/>}/>
+          <Route path="/" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/chat" element={<Chat/>}/>
